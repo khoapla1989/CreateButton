71679dd31b393137c582ed828a809a1d87b6dccc
khoapla1989 <100771704+khoapla1989@users.noreply.github.com>
2022-03-19 18:06:36 +0700

    Add project files.

---
+++ CreateButton/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace CreateButton
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMain = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.AutoSize = true;
+            this.btnMain.Location = new System.Drawing.Point(119, 51);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(239, 41);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "Click To Create New Button ";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.btnMain_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnMain);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+    }
+}
---
+++ CreateButton/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreateButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMain_Click(object sender, EventArgs e)
+        {
+            AddButton();
+        }
+        int i = 0;
+         void AddButton()
+        {
+            Random random = new Random();
+           Button btn = new Button() { 
+               Text =   i.ToString(),  
+               AutoSize = true,
+               Location = new Point(random.Next(0,this.Size.Width),random.Next(0,this.Size.Height))
+           };
+            btn.Click += Btn_Click;
+            this.Controls.Add(btn);
+            i++;
+        }
+
+         void Btn_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button(); 
+            Message.Show(btn.Text);
+        }
+    }
+}
